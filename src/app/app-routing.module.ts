0f9b295f3985a3773395a6f8d0ebbd425d6a46d2
Emilio-Plances <emilio.plances@gmail.com>
2024-03-08 17:03:42 +0100

    Robe varie+ form bellissimi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/home'},
+  { path: 'logSystem', loadChildren: () => import('./pages/log-system/log-system.module').then(m => m.LogSystemModule) },
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'myCharacters', loadChildren: () => import('./pages/my-characters/my-characters.module').then(m => m.MyCharactersModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { LogSystemModule } from './pages/log-system/log-system.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    LogSystemModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
