06a1988f37d72c778040e28a5f9e7ea42c8b952d
Emilio-Plances <emilio.plances@gmail.com>
2024-03-16 17:00:37 +0100

    Creazione interfaccia character

--- src/app/interfaces/enum.ts
+++ src/app/interfaces/enum.ts
@@ -23,3 +23,71 @@ export enum School{
   Transmutation,
   Necromancy
 }
+export enum Status{
+  Public,
+  Private
+}
+export enum Dice{
+  D4,
+  D6,
+  D8,
+  D10,
+  D12,
+  D20
+}
+export enum Race{
+  Dragonborn,
+  Dwarf,
+  Elf,
+  Gnome,
+  Half_elf,
+  Human,
+  Halfling,
+  Half_orc,
+  Tiefling
+}
+export enum Background{
+  Acolyte,
+  Criminal,
+  Folk_hero,
+  Noble,
+  Sage,
+  Soldier
+}
+export enum Alignment{
+  Lawful_good,
+  Neutral_good,
+  Chaotic_good,
+  Lawful_neutral,
+  Neutral_neutral,
+  Chaotic_neutral,
+  Lawful_evil,
+  Neutral_evil,
+  Chaotic_evil
+}
+export enum Skill{
+  Acrobatics,
+  Animal_handling,
+  Arcana,
+  Athletics,
+  Charisma,
+  Constitution,
+  Deception,
+  Dexterity,
+  History,
+  Insight,
+  Intelligence,
+  Intimidation,
+  Investigation,
+  Medicine,
+  Nature,
+  Perception,
+  Performance,
+  Persuasion,
+  Religion,
+  Sleight_of_hand,
+  Stealth,
+  Strenght,
+  Survival,
+  Wisdom
+}
--- src/app/interfaces/i-character.ts
+++ src/app/interfaces/i-character.ts
@@ -1,2 +1,30 @@
+import { Alignment, Background, Classes, Dice, Race, Skill, Status } from "./enum";
+import { ISpell } from "./ispell";
+import { IUser } from "./iuser";
+
 export interface ICharacter {
+  id:number,
+  user:IUser,
+  status:Status,
+  name:string,
+  pgClass:Classes,
+  classArmor:number,
+  dice:Dice,
+  proficiency:number,
+  initiative:number,
+  image:string,
+  race:Race,
+  hp:number,
+  background:Background,
+  alignment:Alignment,
+  backgroundText:string,
+  strenght:number,
+  dexterity:number,
+  constitution:number,
+  intelligence:number,
+  wisdom:number,
+  charisma:number,
+  spells:ISpell[],
+  skills:Skill,
+  preferredUsers:IUser[]
 }
